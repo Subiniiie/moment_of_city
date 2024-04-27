79eb7d445d4a625b8fa368385c768f1722c34682
박수빈 <subin57419@gmail.com>
2024-04-27 23:33:41 +0900

    시계 생성

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,26 @@
 const clockTag = document.querySelector('.clock')
 
-const clock = new Date()
-console.log(clock)
+
+
+function random() {
+    setInterval(() => {
+        const newClock = new Date()
+        let hours = newClock.getHours()
+        if (hours.toString().length === 1) {
+            hours = ['0', hours].join('')
+        }
+        let minutes = newClock.getMinutes()
+        if (minutes.toString().length === 1) {
+            minutes = '0' + minutes
+        }
+        let seconds = newClock.getSeconds()
+        if (seconds.toString().length === 1) {
+            seconds = '0' + seconds
+        }
+        clockTag.textContent = `${hours}:${minutes}:${seconds}`
+    }), 500
+}
+
+
+random()
+
